c10ec593f78fe65ed6afb9bb52c9753d5117e704
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2021-02-05 15:08:48 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import QuesView from "./QuesView";
+import Home from "./Home";
 
+import {Navbar,Nav,NavDropdown,Form,FormControl,Button} from "react-bootstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="#home">Open Source Org Forum</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link  href="/">Home</Nav.Link>
+              <Nav.Link href="/ques_view">Ques Veiw</Nav.Link>
+              <Nav.Link href="/answer">Users</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/ques_view">
+            <QuesView />
+          </Route>
+          <Route path="/answer">
+            <Answer />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+          
+        </Switch>
+    </Router>
   );
 }
 
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Answer() {
+  return <h2>Answer</h2>;
+}
+
+function Login() {
+  return <h1>Login</h1>;
+}
 export default App;
